34008ce34cb5e1cd3e0f6566f299c42e6c98e109
Alex O Regan <alexsoregan@gmail.com>
2019-06-19 23:05:38 +0100

    write code documentation

--- netblend/net.py
+++ netblend/net.py
@@ -6,11 +6,31 @@ Custom Blender object
 """
 class Net:
 
+
+    """
+    'centeroffset1D' takes the length of a layer and returns a starting offset
+        such that the layer will be centered with the net's largest layer.
+    layer_length <int>: the size of the layer to center
+    axis <int>: the index of the axis to center the point on - x: 0, y: 1, z: 2
+    """
+    def __centeroffset1D__(self, layer_length, axis=1):
+        pass
+
+
+    """
+    'centeroffset2D' takes a layer's dimensions and returns a starting offset
+        such that the layer will be centered with the net's largest layer.
+    layer_dimen <tuple[2]>: the y and z dimensions (assuming the x-dimension is 1) of the layer to center
+    """
+    def __centeroffset2D__(self, layer_dimen):
+        pass
+
+
     """
     'make' adds neuron objects to the scene according to the network shape
-    origin: coordinates to begin building from
-    meshfunc: the primitive mesh function to call for each neuron
-    centered: center the neurons around the x-axis, otherwise, align them to branch off the x-axis
+    origin <tuple[3]>: coordinates to begin building from
+    meshfunc <func>: the primitive mesh function to call for each neuron
+    centered <bool>: center layers relative to one-another
     """
     def make(self, origin=(0,0,0), meshfunc=bpy.ops.mesh.primitive_cube_add, centered=False):
         self.neurons = []
@@ -30,6 +50,8 @@ class Net:
 
             elif type(layer) is list:
                 assert type(layer[0]) is int, "Bad layer shape: A 2-d layer must have shape [x, y]"
+
+                # make a grid for 2-d layers
                 for y in range(layer[0]):
                     for z in range(layer[1]):
                         meshfunc( radius=self.neuron_radius, location=tuple(next_neuron_loc) )
@@ -45,8 +67,8 @@ class Net:
 
 
     """
-    Construct a full neural net blender object.
-    Each neuron is a primitive.
+    Construct a full neural net blender object. Each neuron is a primitive object.
+    config <dict>: should contain 'arch' and 'activations_sequence' lists
     """
     def __init__ (self, config):
         self.neuron_radius = 0.015
