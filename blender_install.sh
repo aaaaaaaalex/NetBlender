e1c46d60ffcfead2a288067e460677864977cf12
Alex O Regan <alexsoregan@gmail.com>
2019-06-19 21:09:05 +0100

    update install script to wipe pycache

--- blender_install.sh
+++ blender_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+rm -rf ./netblend/__pycache__
 rm -rf ~/.config/blender/2.79/scripts/modules/netblend
 rm -rf ~/.config/blender/2.79/scripts/modules/__pychache__
 
 mkdir ~/.config/blender/2.79/scripts/modules/netblend
-cp ./netblend/* ~/.config/blender/2.79/scripts/modules/netblend
+cp -r ./netblend/* ~/.config/blender/2.79/scripts/modules/netblend
